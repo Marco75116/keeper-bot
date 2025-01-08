7b6e40c9c15adaa61dbd49ba3f4f6728cff131d0
marc <marc@MacBook-Air-de-marc.local>
2025-01-08 19:58:29 +0100

    init bot telegraf client

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// src/index.ts
+import { bot } from "./utils/clients/telegraf.client";
+
+async function main() {
+ try {
+   bot.command('start', ctx => ctx.reply('Bot started!'));
+   
+   await bot.launch();
+   console.log('Bot is running!');
+ } catch (error) {
+   console.error('Bot launch failed:', error);
+   process.exit(1);
+ }
+}
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+main();
