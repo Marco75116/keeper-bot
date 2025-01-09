73d004fa1c35ab90c68785f7b857b5a6ebee7a58
marc <marc@MacBook-Air-de-marc.local>
2025-01-09 04:52:07 +0100

    delete user message & set up close action for smooth integration

--- src/utils/tg/commands/start.command.ts
+++ src/utils/tg/commands/start.command.ts
@@ -4,6 +4,7 @@ import { WELCOME_MESSAGE } from "../../constants/messages.constant";
 import { getWelcomeKeyboard } from "../keyboards/global.keyboards";
 import { createUser } from "../../helpers/global.helper";
 import { getUser } from "../../helpers/bddqueries/get.queries.helper";
+import { WELCOME_ACTIONS } from "../actions/global.actions";
 
 export const botStart = () => {
   bot.on(message("text"), async (ctx) => {
@@ -15,6 +16,12 @@ export const botStart = () => {
       createUser(ctx.update.message.from);
     }
 
+    await ctx.deleteMessage(ctx.update.message.message_id);
     await ctx.reply(WELCOME_MESSAGE, getWelcomeKeyboard());
+  });
+
+  bot.action(WELCOME_ACTIONS.CLOSE, async (ctx) => {
+    await ctx.answerCbQuery();
+    await ctx.deleteMessage(ctx.update.callback_query.message?.message_id);
   });
 };
