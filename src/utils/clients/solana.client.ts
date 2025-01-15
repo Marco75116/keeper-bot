61e08d5bd553afa8b4a30655353866ac9d44b094
marc-antoine ourradour <ourradourm@gmail.com>
2025-01-15 16:18:55 +0100

    add tonweb + add env variables

--- src/utils/clients/solana.client.ts
+++ src/utils/clients/solana.client.ts
@@ -1,10 +1,17 @@
 import { Connection } from "@solana/web3.js";
-import { SOLANA_ENDPOINT } from "../constants/global.constant";
+
+export const SOLANA_ENDPOINT = Bun.env.SOLANA_ENDPOINT!;
+
+if (!SOLANA_ENDPOINT) {
+  throw new Error("SOLANA_ENDPOINT not found in environment variables");
+}
 
 export const solanaClient = new Connection(SOLANA_ENDPOINT, "confirmed");
 
 export const PROJECT_WALLET = Bun.env.SOL_PROJECT_WALLET_1!;
 
 if (!PROJECT_WALLET) {
-  throw new Error("Project wallet address not found in environment variables");
+  throw new Error(
+    "SOLANA Project wallet address not found in environment variables"
+  );
 }
--- src/utils/clients/ton.client.ts
+++ src/utils/clients/ton.client.ts
@@ -1,12 +1,28 @@
 import { TonClient } from "@ton/ton";
 import { TON_ENDPOINT } from "../constants/global.constant";
+import TonWeb from "tonweb";
+
+export const TON_API_KEY = Bun.env.TON_API_KEY!;
+
+if (!TON_API_KEY) {
+  throw new Error("TON_API_KEY not found in environment variables");
+}
 
 export const tonClient = new TonClient({
   endpoint: TON_ENDPOINT,
+  apiKey: TON_API_KEY,
 });
+
+export const tonweb = new TonWeb(
+  new TonWeb.HttpProvider(TON_ENDPOINT, {
+    apiKey: Bun.env.TON_API_KEY,
+  })
+);
 
 export const TON_PROJECT_WALLET = Bun.env.TON_PROJECT_WALLET_1!;
 
 if (!TON_PROJECT_WALLET) {
-  throw new Error("Project wallet address not found in environment variables");
+  throw new Error(
+    "TON Project wallet address not found in environment variables"
+  );
 }
